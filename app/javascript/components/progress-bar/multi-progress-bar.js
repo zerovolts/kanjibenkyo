6d8e5f311c55954ef23ccc796c9d8e9e9acd1020
Zach Stone <hexaphixylix@yahoo.com>
2018-09-08 01:37:44 -0400

    fix progress bar on show page

--- app/javascript/components/progress-bar/multi-progress-bar.js
+++ app/javascript/components/progress-bar/multi-progress-bar.js
@@ -3,9 +3,14 @@ import React from "react";
 import "./progress-bar.scss";
 
 const MultiProgressBar = ({ sections }) => {
-  const colorSections = sections.map(section => (
-    <div style={{ background: section.color, width: section.percent + "%" }} />
-  ));
+  const colorSections = sections
+    // filter so that there aren't empty sections (breaks border-radius css)
+    .filter(section => section.percent > 0)
+    .map(section => (
+      <div
+        style={{ background: section.color, width: section.percent + "%" }}
+      />
+    ));
 
   return (
     <div
